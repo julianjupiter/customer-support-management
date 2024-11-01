ec971a33a42eaa2206f6b2857b0dfba662751e43
Julian Jupiter <julianjupiter.io@gmail.com>
2024-11-02 03:36:46 +0800

    Initial codes

---
+++ csm-service/src/main/java/com/julianjupiter/csm/CsmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.julianjupiter.csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmServiceApplication.class, args);
+	}
+
+}
---
+++ csm-service/src/test/java/com/julianjupiter/csm/CsmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianjupiter.csm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
